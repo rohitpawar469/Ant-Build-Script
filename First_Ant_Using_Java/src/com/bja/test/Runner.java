0d281ecfa30da0f701cb66357525d26f099f08a5
rohit <rohit@DESKTOP-F16QUPD>
2020-08-08 20:04:25 +0530

    First Ant Build

---
+++ First_Ant_Using_Java/src/com/bja/test/Runner.java
@@ -0,0 +1,9 @@
+package com.bja.test;
+
+public class Runner 
+{
+	public static void main(String[] args) 
+	{
+		System.out.println("Running Java Program with Ant Integration... With Modification");
+	}
+}
