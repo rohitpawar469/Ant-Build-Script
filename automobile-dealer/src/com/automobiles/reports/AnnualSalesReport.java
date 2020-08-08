61c4d2a447dc475e771b4b02b4afed7f3b46509d
rohit <rohit@DESKTOP-F16QUPD>
2020-08-08 20:06:29 +0530

    Complete Example of Compile,Package Deploy

---
+++ automobile-dealer/src/com/automobiles/reports/AnnualSalesReport.java
@@ -0,0 +1,9 @@
+package com.automobiles.reports;
+
+public class AnnualSalesReport 
+{
+	public static void main(String[] args) 
+	{
+		System.out.println("Successfully Generated the Reports..");
+	}
+}
